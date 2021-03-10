eb2c072e23ed6fea7034039fc88715b4e29375a2
RusAnonym <alexandrsemin2033@gmail.com>
2021-03-11 01:16:17 +0300

    add cookieAlert

--- src/TSX/app.tsx
+++ src/TSX/app.tsx
@@ -1,11 +1,38 @@
-import { Nav, Navbar } from "react-bootstrap";
+import { useCookies } from "react-cookie";
+
+import { Nav, Navbar, Alert, Button } from "react-bootstrap";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Main from "./pages/main";
 import Schedule from "./pages/schedule";
 import Replacements from "./pages/replacements";
 
-export default function App() {
+function CookieAlert(callbackFunction: any) {
+	return (
+		<Alert variant="success">
+			<Alert.Heading>
+				Этот сайт использует cookie для хранения данных.
+			</Alert.Heading>
+			<p>
+				Продолжая использовать сайт, Вы даёте согласие на работу с этими
+				файлами.
+			</p>
+			<hr />
+			<div>
+				<Button
+					onClick={() => {
+						callbackFunction("accept", true, { path: "/" });
+					}}
+					variant="outline-success"
+				>
+					Принять и закрыть
+				</Button>
+			</div>
+		</Alert>
+	);
+}
+
+function ShowMenu() {
 	return (
 		<Router>
 			<Navbar bg="dark" expand="lg">
@@ -24,4 +51,9 @@ export default function App() {
 			<Route path="/replacements" component={Replacements} />
 		</Router>
 	);
+}
+
+export default function App() {
+	const [cookies, setCookie] = useCookies(["accept"]);
+	return cookies.accept ? <ShowMenu /> : CookieAlert(setCookie);
 }
